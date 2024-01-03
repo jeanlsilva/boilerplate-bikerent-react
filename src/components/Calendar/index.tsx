db5ba67e9f3dd47a0b93ee73c096a4b5d8cfb2e6
jean <jeanslucasilva@gmail.comm>
2024-01-02 22:33:29 -0500

    fix select date for different months

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -6,13 +6,10 @@ import Paper from '@mui/material/Paper';
 export function Calendar() {
   const [selectedStartDate, setSelectedStartDate] = useState<Date | undefined>(undefined);
   const [selectedEndDate, setSelectedEndDate] = useState<Date | undefined>(undefined);
+  const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth())
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear())
 
   const handleDateClick = (date: Date) => {
-    // if no start date or end date selected, set start date
-    // if start date selected and end date not selected and selected date is after start date, set end date
-    // if start date selected and end date not selected and selected date is before start date, set start date
-    // if start date selected and end date selected and selected date is before start date, set start date
-        // if start date selected and end date selected and selected date is after start date, set end date
     if (!selectedStartDate && !selectedEndDate) {
         setSelectedStartDate(date);
     }
@@ -37,23 +34,37 @@ export function Calendar() {
   };
 
   const goToNextMonth = () => {
-    console.log('go to next month')
+    setSelectedMonth(selectedMonth + 1)
   };
 
   const goToPreviousMonth = () => {
-    console.log('go to previous month')
+    setSelectedMonth(selectedMonth - 1)
   };
+
+  const isInsideRange = (day: number) => {
+    const date = new Date(selectedYear, selectedMonth, day);
+    return selectedStartDate && !selectedEndDate ? 
+            (date.getTime() === selectedStartDate.getTime() ? { background: '#C1CFF2', color: '#1F49D1', borderRadius: '50%' } : {})
+        : selectedStartDate && selectedEndDate ? 
+            (
+                date.getTime() === selectedStartDate.getTime()
+                ? { background: '#C1CFF2', color: '#1F49D1', borderTopLeftRadius: '50%', borderBottomLeftRadius: '50%' }
+                : date.getTime() === selectedEndDate.getTime()
+                ? { background: '#C1CFF2', color: '#1F49D1', borderTopRightRadius: '50%', borderBottomRightRadius: '50%' }
+                : date.getTime() > selectedStartDate.getTime() && date.getTime() < selectedEndDate.getTime()
+                ? { background: '#C1CFF2', borderRadius: 0 } : {}
+            ) : { background: '#1F49D1', color: '#fff' }
+}
 
   const RenderCalendar = () => {
     const weekdays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 
     const year = new Date().getFullYear();
-    const month = new Date().getMonth();
 
-    const daysInMonth = new Date(year, month + 1, 0).getDate();
-    const firstDay = new Date(year, month, 1).getDay();
-    const previousDay = new Date(year, month, 0).getDate();
-    const monthAsText = new Date(year, month, 1).toLocaleString('default', { month: 'long' });
+    const daysInMonth = new Date(year, selectedMonth + 1, 0).getDate();
+    const firstDay = new Date(year, selectedMonth, 1).getDay();
+    const previousDay = new Date(year, selectedMonth, 0).getDate();
+    const monthAsText = new Date(year, selectedMonth, 1).toLocaleString('en-US', { month: 'long' });
 
     const weekHeader = weekdays.map((day, index) => (
       <Box key={index} borderBottom={0}>
@@ -83,7 +94,7 @@ export function Calendar() {
     }
 
     for (let i = 1; i <= daysInMonth; i++) {
-      const date = new Date(year, month, i);
+      const date = new Date(year, selectedMonth, i);
 
       calendarDays.push(
         <Item
@@ -113,56 +124,44 @@ export function Calendar() {
       );
     }
 
-    const isInsideRange = (day: number) => {
-        const date = new Date(year, month, day);
-        return selectedStartDate && !selectedEndDate ? 
-                (date.getTime() === selectedStartDate.getTime() ? { background: '#C1CFF2', color: '#1F49D1', borderRadius: '50%' } : {})
-            : selectedStartDate && selectedEndDate ? 
-                (
-                    date.getTime() === selectedStartDate.getTime()
-                    ? { background: '#C1CFF2', color: '#1F49D1', borderTopLeftRadius: '50%', borderBottomLeftRadius: '50%' }
-                    : date.getTime() === selectedEndDate.getTime()
-                    ? { background: '#C1CFF2', color: '#1F49D1', borderTopRightRadius: '50%', borderBottomRightRadius: '50%' }
-                    : date.getTime() > selectedStartDate.getTime() && date.getTime() < selectedEndDate.getTime()
-                    ? { background: '#C1CFF2', borderRadius: 0 } : {}
-                ) : { background: '#1F49D1', color: '#fff' }
-    }
-
     const dividedCalendarDays: JSX.Element[] = []
     for (let i = 0; i <= calendarDays.length; i += 7) {
         dividedCalendarDays.push(<Grid container spacing={0}>
-            <Grid item xs={12/7} sx={() => isInsideRange(i)}>{calendarDays[i]}</Grid>
-            <Grid item xs={12/7} sx={() => isInsideRange(i + 1)}>{calendarDays[i + 1]}</Grid>
-            <Grid item xs={12/7} sx={() => isInsideRange(i + 2)}>{calendarDays[i + 2]}</Grid>
-            <Grid item xs={12/7} sx={() => isInsideRange(i + 3)}>{calendarDays[i + 3]}</Grid>
-            <Grid item xs={12/7} sx={() => isInsideRange(i + 4)}>{calendarDays[i + 4]}</Grid>
-            <Grid item xs={12/7} sx={() => isInsideRange(i + 5)}>{calendarDays[i + 5]}</Grid>
-            <Grid item xs={12/7} sx={() => isInsideRange(i + 6)}>{calendarDays[i + 6]}</Grid>
+            <Grid item xs={12/7} sx={() => isInsideRange((i - firstDay + 1))}>{calendarDays[i]}</Grid>
+            <Grid item xs={12/7} sx={() => isInsideRange((i - firstDay + 1) + 1)}>{calendarDays[i + 1]}</Grid>
+            <Grid item xs={12/7} sx={() => isInsideRange((i - firstDay + 1) + 2)}>{calendarDays[i + 2]}</Grid>
+            <Grid item xs={12/7} sx={() => isInsideRange((i - firstDay + 1) + 3)}>{calendarDays[i + 3]}</Grid>
+            <Grid item xs={12/7} sx={() => isInsideRange((i - firstDay + 1) + 4)}>{calendarDays[i + 4]}</Grid>
+            <Grid item xs={12/7} sx={() => isInsideRange((i - firstDay + 1) + 5)}>{calendarDays[i + 5]}</Grid>
+            <Grid item xs={12/7} sx={() => isInsideRange((i - firstDay + 1) + 6)}>{calendarDays[i + 6]}</Grid>
         </Grid>)
     }
 
     return (
       <Box borderRadius={1} p={2} mb={4} bgcolor='#1F49D1' color='#fff'>
         <Box display='flex' alignItems='center' justifyContent='space-between' gap={2} mb={2}>
-        <Typography variant='h6'sx={{ fontSize: '1.25rem', fontWeight: 500 }}>
-            {monthAsText} {year}
-        </Typography>
+          <Box>
+          <Typography variant='h6'sx={{ fontSize: '1.625rem', fontWeight: 700 }}>
+              {monthAsText}
+          </Typography>
+          <Typography>{year}</Typography>
+        </Box>
           {goToPreviousMonth ? (
             <Button
                 variant='outlined' 
                 sx={{ 
-                    border: '1px solid #fff',
-                    ...(month === new Date().getMonth() && year === new Date().getFullYear() && { pointerEvents: 'none' }), // example inline style
+                  border: '1px solid #fff', width: 52, height: 52, borderRadius: '10px',
+                    ...(selectedMonth === new Date().getMonth() && year === new Date().getFullYear() && { pointerEvents: 'none' }), // example inline style
                 }}
               onClick={goToPreviousMonth}
             >
-              <Typography color='white'>{'<'}</Typography>
+              <Typography color='white' display='block'>{'<'}</Typography>
             </Button>
           ) : (
             <Box visibility='hidden'></Box>
           )}
           {goToNextMonth ? (
-            <Button variant='outlined' sx={{ border: '1px solid #fff' }} onClick={goToNextMonth}>
+            <Button variant='outlined' sx={{ border: '1px solid #fff', width: 52, height: 52, borderRadius: '10px' }} onClick={goToNextMonth}>
               <Typography color='white'>{'>'}</Typography>
             </Button>
           ) : (
